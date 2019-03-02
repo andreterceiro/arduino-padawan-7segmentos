8af401bd41c69f1314118abb6da9812950f1f168
terceiro.com.br <andre@terceiro.com.br>
2019-03-01 21:42:40 -0300

    Update 7_segmentos.ino

--- 7_segmentos.ino
+++ 7_segmentos.ino
@@ -1,36 +1,101 @@
+const int A = A4;
+const int B = A5;
+const int C = 7;
+const int D = 8;
+const int E = 5;
+const int F = 3;
+const int G = 6;
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(A4, OUTPUT);
-  pinMode(A5, OUTPUT);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(A4, LOW);
-  digitalWrite(A5, LOW);
+  // 1
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
 
   delay(1000);
 
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(A4, LOW);
-  digitalWrite(A5, LOW);
+  // 2
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, HIGH);
 
   delay(1000);
 
+  // 3
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+
+  delay(1000);
+
+  // 4
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+
+  delay(1000);
+
+  // 5
+  digitalWrite(A, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+
+  delay(1000);
+
+  // 6
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
+
+  delay(1000);
+
+  // 7
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+
+  delay(1000);
+
+  // 8
   digitalWrite(3, HIGH);
   digitalWrite(5, HIGH);
   digitalWrite(6, HIGH);
@@ -40,64 +105,4 @@ void loop() {
   digitalWrite(A5, HIGH);
 
   delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, LOW);
-  digitalWrite(A4, LOW);
-  digitalWrite(A5, LOW);
-
-  delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(A4,HIGH);
-  digitalWrite(A4, LOW);
-  digitalWrite(A4, LOW);
-
-  delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(A4, HIGH);
-  digitalWrite(A5, LOW);
-
-  delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(A5, LOW);
-  digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(A4, LOW);
-
-  delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(A4, HIGH);
-  digitalWrite(A5, HIGH);
-
-  delay(1000);
-
-  digitalWrite(3, HIGH);
-  digitalWrite(5, HIGH);
-  digitalWrite(6, HIGH);
-  digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  digitalWrite(A4, HIGH);
-  digitalWrite(A5, LOW);
-
 }
